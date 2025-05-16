3b230c7f7565d6c2c9561b98340d17d8b68504df
bilaltanveer7 <bilaltanveer498@gmail.com>
2025-05-16 13:14:02 +0500

    attendance screens completed

--- routes/web.php
+++ routes/web.php
@@ -108,6 +108,10 @@ Route::middleware(['auth:web', 'verified'])->group(function () {
     Route::get('/employee/list', function () {
         return Inertia::render('App/Admin/Employee/EmployeeList');
     })->name('employee.employeeList');
+
+    Route::get('/employee/monthly/attendance/report', function () {
+        return Inertia::render('App/Admin/Employee/MonthlyReport');
+    })->name('employee.monthlyreport');
     // member
     // Route::get('/members', [MembersController::class, 'index'])->name('members.index');
     // Route::resource('/members/member-types', MemberTypeController::class)->except('show', 'edit');
@@ -178,123 +182,6 @@ Route::middleware(['auth:web', 'verified'])->group(function () {
     Route::post('/user-member/store', [UserMemberController::class, 'store'])->name('usermember.store');
 });
 
-<<<<<<< HEAD
-        Route::get('/employee/list', function () {
-            return Inertia::render('App/Admin/Employee/EmployeeList');
-        })->name('employee.employeeList');
-
-        Route::get('/employee/department/list', function () {
-            return Inertia::render('App/Admin/Employee/Department');
-        })->name('employee.departmentlist');
-
-        Route::get('/employee/attendance/dashboard', function () {
-            return Inertia::render('App/Admin/Employee/Attendance');
-        })->name('employee.attendance');
-
-        Route::get('/employee/attendance/add/leave/application', function () {
-            return Inertia::render('App/Admin/Employee/AddLeave');
-        })->name('employee.addleave');
-
-        Route::get('/employee/all/attendance', function () {
-            return Inertia::render('App/Admin/Employee/AllAttendance');
-        })->name('employee.allattendance');
-
-        Route::get('/employee/leave/category', function () {
-            return Inertia::render('App/Admin/Employee/Category');
-        })->name('employee.leavecategory');
-
-        Route::get('/employee/add/leave/category', function () {
-            return Inertia::render('App/Admin/Employee/LeaveCategory');
-        })->name('employee.addleavecategory');
-
-        Route::get('/employee/leave/application/management', function () {
-            return Inertia::render('App/Admin/Employee/LeaveManage');
-        })->name('employee.leavemanagement');
-
-        Route::get('/employee/leave/report', function () {
-            return Inertia::render('App/Admin/Employee/Report');
-        })->name('employee.leavereport');
-
-        Route::get('/employee/attendance/report', function () {
-            return Inertia::render('App/Admin/Employee/AttendanceReport');
-        })->name('employee.attendancereport');
-
-        Route::get('/employee/attendance/monthly/report', function () {
-            return Inertia::render('App/Admin/Employee/MonthlyReport');
-        })->name('employee.monthlyreport');
-
-        //Membership Booking Routes
-        Route::get('/membership/booking/dashboard', function () {
-            return Inertia::render('App/Admin/Membership/Dashboard');
-        })->name('membership.dashboard');
-
-        Route::get('/admin/add/personal/information', function () {
-            return Inertia::render('App/Admin/Membership/AddForm-1');
-        })->name('membership.add');
-
-        Route::get('/admin/add/contact/information', function () {
-            return Inertia::render('App/Admin/Membership/AddForm-2');
-        })->name('membership.add');
-
-        Route::get('/admin/add/membership/information', function () {
-            return Inertia::render('App/Admin/Membership/AddForm-3');
-        })->name('membership.add');
-
-        Route::get('/admin/membership/history', function () {
-            return Inertia::render('App/Admin/Membership/History');
-        })->name('membership.history');
-
-        Route::get('/admin/membership/guest/history', function () {
-            return Inertia::render('App/Admin/Membership/Guest');
-        })->name('membership.guest');
-
-        Route::get('/admin/membership/add/guest', function () {
-            return Inertia::render('App/Admin/Membership/AddGuest');
-        })->name('membership.addguest');
-
-        Route::get('/admin/guest/visit/detail', function () {
-            return Inertia::render('App/Admin/Membership/Checkout');
-        })->name('membership.checkout');
-
-        Route::get('/admin/all/members', function () {
-            return Inertia::render('App/Admin/Membership/Members');
-        })->name('membership.members');
-
-        Route::get('/admin/membership/visit/detail', function () {
-            return Inertia::render('App/Admin/Membership/Detail');
-        })->name('membership.detail');
-
-        Route::get('/admin/membership/finance', function () {
-            return Inertia::render('App/Admin/Membership/Finance');
-        })->name('membership.finance');
-
-        Route::get('/admin/membership/all.members', function () {
-            return Inertia::render('App/Admin/Membership/MemberType');
-        })->name('membership.membertype');
-
-        Route::get('/admin/membership/add/membertype', function () {
-            return Inertia::render('App/Admin/Membership/AddMember');
-        })->name('membership.addmembertype');
-
-        Route::get('/admin/membership/all/payments', function () {
-            return Inertia::render('App/Admin/Membership/AllPayment');
-        })->name('membership.allpayment');
-
-        Route::get('/admin/membership/full/detail', function () {
-            return Inertia::render('App/Admin/Membership/CompleteDetail');
-        })->name('membership.detail');
-
-        // tenant route
-        Route::get('tenants', [TenantController::class, 'index'])->name('tenant.index');
-        Route::get('tenant/register', [TenantController::class, 'create'])->name('tenant.create');
-        Route::post('tenant/store', [TenantController::class, 'store'])->name('tenant.store');
-    });
-
-    require __DIR__ . '/settings.php';
-    require __DIR__ . '/auth.php';
-}
-=======
 // Central guest-only auth routes
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
->>>>>>> 6a7523aa728c57dd7444263ee0388f367607cb51
