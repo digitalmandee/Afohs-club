448a2d0661fa0eef5ccee096b613333c785494cc
usama <usama@wodwes.us>
2025-06-12 19:09:48 +0500

    update

--- resources/js/pages/App/Admin/Booking/Dashboard.jsx
+++ resources/js/pages/App/Admin/Booking/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Card, Button, Form, Badge, Modal } from 'react-bootstrap';
 import { Search, FilterAlt } from '@mui/icons-material';
-import { ThemeProvider, createTheme, Box, Typography } from '@mui/material';
+import { ThemeProvider, createTheme, Box, Typography, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { router } from '@inertiajs/react';
 import SideNav from '@/components/App/AdminSideBar/SideNav';
 import DatePicker from "react-multi-date-picker";
@@ -154,9 +154,9 @@ const bookingsData = [
 ];
 
 const CustomDateRangePicker = ({ adults, setAdults, onSearch }) => {
+    const [bookingType, setBookingType] = useState('room');
     const [values, setValues] = useState([
         new DateObject().subtract(4, "days"),
-        new DateObject().add(4, "days")
     ]);
     const [showGuestsModal, setShowGuestsModal] = useState(false);
 
@@ -178,7 +178,20 @@ const CustomDateRangePicker = ({ adults, setAdults, onSearch }) => {
 
     return (
         <div style={{ padding: '10px', borderRadius: '4px' }}>
-            <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+            <div style={{ display: 'grid', gridTemplateColumns: '200px 1fr 1fr 120px', gap: '10px', alignItems: 'center', marginBottom: '10px' }}>
+                <FormControl >
+                    <InputLabel id="booking-label">Booking Type</InputLabel>
+                    <Select
+                        labelId="booking-label"
+                        id="booking-select"
+                        value={bookingType}
+                        label="Booking Type"
+                    // onChange={handleChange}
+                    >
+                        <MenuItem value="room">Room</MenuItem>
+                        <MenuItem value="event">Event</MenuItem>
+                    </Select>
+                </FormControl>
                 <div
                     style={{
                         flex: 1,
@@ -193,7 +206,9 @@ const CustomDateRangePicker = ({ adults, setAdults, onSearch }) => {
                 >
                     <span>📅</span>
                     <DatePicker
+                        placeholder="CheckIn to CheckOut"
                         value={values}
+                        dateSeparator=" to "
                         onChange={handleRangeSelect}
                         range
                         rangeHover
@@ -592,9 +607,9 @@ const BookingDashboard = ({ data }) => {
                                             <Col md={10}>
                                                 <div className="d-flex justify-content-between align-items-center mb-3 flex-wrap">
                                                     <div>
-                                                        <Typography style={{ fontWeight: 500, fontSize: '20px', color: '#121212' }}>{booking.type}</Typography>
+                                                        <Typography style={{ fontWeight: 500, fontSize: '20px', color: '#121212' }}>{booking.booking_type}</Typography>
                                                         <Typography variant="body2" style={{ color: '#7F7F7F', fontSize: '14px', fontWeight: 400 }}>
-                                                            Created on {booking.checkid}
+                                                            Created on {booking.checkin}
                                                         </Typography>
                                                     </div>
                                                     <Badge
@@ -622,7 +637,7 @@ const BookingDashboard = ({ data }) => {
                                                             Booking ID
                                                         </Typography>
                                                         <Typography variant="body1" style={{ fontWeight: 400, color: '#121212', fontSize: '12px' }}>
-                                                            {booking.bookingId}
+                                                            {booking.booking_id}
                                                         </Typography>
                                                     </Col>
                                                     <Col md={4} sm={6} className="mb-2">
@@ -654,7 +669,7 @@ const BookingDashboard = ({ data }) => {
                                                             Adults
                                                         </Typography>
                                                         <Typography variant="body1" style={{ fontWeight: 400, color: '#121212', fontSize: '12px' }}>
-                                                            {booking.adults}
+                                                            {booking.persons}
                                                         </Typography>
                                                     </Col>
                                                 </Row>
