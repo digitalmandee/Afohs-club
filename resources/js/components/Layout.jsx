ad61e8fbb6fcb35cc7eccdc4b3de35f6646a5479
bilaltanveer7 <bilaltanveer498@gmail.com>
2025-11-03 15:42:52 +0500

    final changes

--- resources/js/components/Layout.jsx
+++ resources/js/components/Layout.jsx
@@ -8,18 +8,15 @@ export default function Layout({ children }) {
     const [open, setOpen] = useState(true);
 
     return (
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: 'flex', overflowX: 'hidden'}}>
             <SideNav open={open} setOpen={setOpen} />
             <div
                 style={{
-                    //   flexGrow: 1,
-                    //   marginLeft: open ? `${drawerWidthOpen}px` : `${drawerWidthClosed}px`,
-                    //   transition: 'margin-left 0.3s ease-in-out',
-                    //   marginTop: '5rem', // keep your marginTop if needed
                     flexGrow: 1,
                     marginLeft: 0, // <-- set to 0, let flexbox handle the width
                     marginTop: '5rem',
                     transition: 'margin-left 0.3s ease-in-out',
+                    overflowX: 'hidden',
                 }}
             >
                 {children}
